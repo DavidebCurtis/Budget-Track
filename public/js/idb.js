15e80e1439e5ff163f89e5f3578aba0b0667001c
david curtis <heythereimdavidcurtis@gmail.com>
2022-05-15 17:27:08 -0400

    typo in idb addressed

--- public/js/idb.js
+++ public/js/idb.js
@@ -7,7 +7,7 @@ const request = indexedDB.open('budget-tracker', 1);
 request.onupgradeneeded = function (event) {
   // save a reference to the database
   const db = event.target.result;
-  // create an object store (table) called `new_pizza`, set it to have an auto incrementing primary key of sorts
+  // create an object store (table) called `new_budget_input`, set it to have an auto incrementing primary key of sorts
   db.createObjectStore('new_budget_input', { autoIncrement: true });
 };
 
@@ -16,7 +16,7 @@ request.onsuccess = function (event) {
   // when db is successfully created with its object store (from onupgradedneeded event above) or simply established a connection, save reference to db in global variable
   db = event.target.result;
 
-  // check if app is online, if yes run uploadPizza() function to send all local db data to api
+  // check if app is online, if yes run uploadBudget() function to send all local db data to api
   if (navigator.onLine) {
     // we haven't created this yet, but we will soon, so let's comment it out for now
     uploadBudget();
@@ -32,11 +32,11 @@ function saveRecord(record) {
   // open a new transaction with the database with read and write permissions
   const transaction = db.transaction(['new_budget_input'], 'readwrite');
 
-  // access the object store for `new_pizza`
-  const pizzaObjectStore = transaction.objectStore('new_budget_input');
+  // access the object store for `new_budget_input`
+  const budgetObjectStore = transaction.objectStore('new_budget_input');
 
   // add record to your store with add method
-  pizzaObjectStore.add(record);
+  budgetObjectStore.add(record);
 }
 
 function uploadBudget() {
@@ -44,10 +44,10 @@ function uploadBudget() {
   const transaction = db.transaction(['new_budget_input'], 'readwrite');
 
   // access your object store
-  const pizzaObjectStore = transaction.objectStore('new_budget_input');
+  const budgetObjectStore = transaction.objectStore('new_budget_input');
 
   // get all records from store and set to a variable
-  const getAll = pizzaObjectStore.getAll();
+  const getAll = budgetObjectStore.getAll();
 
   // upon a successful .getAll() execution, run this function
   getAll.onsuccess = function () {
@@ -73,7 +73,7 @@ function uploadBudget() {
           // clear all items in your store
           budgetObjectStore.clear();
 
-          alert('All saved pizza has been submitted!');
+          alert('All saved budget has been submitted!');
         })
         .catch((err) => {
           console.log(err);
