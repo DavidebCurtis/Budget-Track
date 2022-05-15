3fcc0a6c0ab293118429da3c93cb0c19381c1b99
david curtis <heythereimdavidcurtis@gmail.com>
2022-05-15 17:45:15 -0400

    added script top index.html

--- public/service-workers.js
+++ public/service-workers.js
@@ -1,1 +1,16 @@
 const FILES_TO_CACHE = ['/', '/index.html', '/index.js', 'styles.css'];
+
+const APP_PREFIX = 'Budget-Tracker-';
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log('installing cache : ' + CACHE_NAME);
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('install', function (e) {});
